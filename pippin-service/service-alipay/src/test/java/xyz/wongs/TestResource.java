c0d78c2f0d90008e23e0588a83846c2975a2dcd5
WONGS <WCNGS@QQ.COM>
2021-09-23 17:15:26 +0800

    修正文件

--- pippin-service/service-alipay/src/test/java/xyz/wongs/TestResource.java
+++ pippin-service/service-alipay/src/test/java/xyz/wongs/TestResource.java
@@ -1,22 +1,18 @@
 package xyz.wongs;
 
-import java.io.File;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 
 public class TestResource {
 
     public static void main(String[] args) throws Exception{
-//        File folder =  new File(Thread.currentThread().getContextClassLoader().getResource("static/images").getPath());
-//        File folder = ResourceUtils.getFile("static/images");
-        File folder =  new File("D:/Alipay/Images");
-        File[] list = folder.listFiles();
-        System.out.println("文件内容为空"+folder.getAbsolutePath());
-        if(list==null || list.length==0){
-            return;
-        }
-        for (File file : list) {
-            System.out.println(file.getName());
-        }
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("jiohriwebar");
+        String content = "wongs";
+        String encrypt =encryptor.encrypt(content);
+        System.out.println("[加密]"+encrypt);
 
+        String decrypt = encryptor.decrypt(encrypt);
+        System.out.println("[解密]"+decrypt);
 
     }
 }
