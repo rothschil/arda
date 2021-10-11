d70c3ac748f0c7ac422896452642435c8ab618a6
WONGS <WCNGS@QQ.COM>
2021-10-11 14:44:04 +0800

    None

--- pippin-service/service-alipay/src/main/java/xyz/wongs/drunkard/mail/web/MailController.java
+++ pippin-service/service-alipay/src/main/java/xyz/wongs/drunkard/mail/web/MailController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,6 +16,7 @@ import xyz.wongs.drunkard.base.vo.MailVo;
  * @date 2021/10/9 - 15:07
  * @since 1.0.0
  */
+@RequestMapping("mail")
 @Controller
 public class MailController {
 
@@ -29,10 +31,20 @@ public class MailController {
         return mv;
     }
 
-    @PostMapping("/send")
-    public void send(MailVo mailVo, @RequestParam("files") MultipartFile[] files) {
+    /** 多文件上传
+     * @author <a href="https://github.com/rothschil">Sam</a>
+     * @date 2021/10/11-14:43
+     * @param mailVo    邮件实体
+     * @param files 文件列表
+     * @return ModelAndView
+     **/
+    @PostMapping(value ="/send")
+    public ModelAndView send(MailVo mailVo, @RequestParam("files") MultipartFile[] files) {
         mailVo.setMultipartFiles(files);
         mailService.send(mailVo);
+        ModelAndView mv = new ModelAndView("mail");
+        mv.addObject("from", mailService.getMailSendFrom());
+        return mv;
     }
 
 }
