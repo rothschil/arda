bf90ccdc47fd535b8ed5e2d4b5ccf58975dfccdf
WCNGS <WCNGS@HOTMAIL.COM>
2021-12-18 21:46:46 +0800

    None

--- common-utils/src/main/java/io/github/rothschil/base/aop/queue/AbstractQueue.java
+++ common-utils/src/main/java/io/github/rothschil/base/aop/queue/AbstractQueue.java
@@ -1,7 +1,6 @@
 package io.github.rothschil.base.aop.queue;
 
 import io.github.rothschil.base.aop.queue.handler.TaskHandlerble;
-import io.github.rothschil.common.utils.thread.ThreadPoolsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
