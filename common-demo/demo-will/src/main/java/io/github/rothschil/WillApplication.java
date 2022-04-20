235626e5fef5da8a25332bd26966526e4198f975
SamAbram <wcngs@hotmail.com>
2022-04-20 15:06:50 +0800

    None

--- common-demo/demo-will/src/main/java/io/github/rothschil/WillApplication.java
+++ common-demo/demo-will/src/main/java/io/github/rothschil/WillApplication.java
@@ -1,6 +1,5 @@
 package io.github.rothschil;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2021/11/6 - 21:27
  * @since 1.0.0
  */
-@MapperScan("io.github.rothschil.**.mapper")
 @SpringBootApplication
 public class WillApplication {
 
