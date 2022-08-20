a8f9ed62f157856e61c76dc2f41e0b7eb3263ecb
SamAbram <wcngs@hotmail.com>
2022-08-20 15:33:06 +0800

    None

---
+++ common-persistence/persistence-elasticsearch/src/test/java/io/github/rothschil/TestEs.java
@@ -0,0 +1,13 @@
+package io.github.rothschil;
+
+
+public class TestEs {
+
+
+    @Test
+    public void testCreatIndex(){
+        ElasticIndexManger elasticIndexManger= new ElasticIndexManger();
+
+        elasticIndexManger.
+    }
+}
