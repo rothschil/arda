b02a37d62ce85280179ddedd0ad7284473e98b3b
Sam <WCNGS@HOTMAIL.COM>
2023-11-16 17:07:03 +0800

    NONE

---
+++ src/main/java/io/github/rothschil/RothschilCommonApplication.java
@@ -0,0 +1,13 @@
+package io.github.rothschil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RothschilCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RothschilCommonApplication.class, args);
+	}
+
+}
