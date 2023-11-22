09a205caad0d8403388a752afa93eeef2cfd30d7
Sam <WCNGS@HOTMAIL.COM>
2023-11-22 15:21:35 +0800

    NONE

--- src/main/java/io/github/rothschil/RothschilApplication.java
+++ src/main/java/io/github/rothschil/RothschilApplication.java
@@ -1,15 +1,19 @@
 package io.github.rothschil;
 
 import io.github.rothschil.common.config.Version;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
+@Slf4j
 @SpringBootApplication
 public class RothschilApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RothschilApplication.class, args);
-		Version.printlnVersionInfo();
+		SpringApplication app =new SpringApplication(RothschilApplication.class);
+		Environment env = app.run(args).getEnvironment();
+		Version.printlnVersionInfo(env);
 	}
 
 }
