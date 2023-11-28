78e7d932df8042868e2de81795a40d7f84f77f61
SamAbram <WCNGS@HOTMAIL.COM>
2023-11-28 16:40:34 +0800

    NONE

--- src/main/java/io/github/rothschil/domain/entity/Intf.java
+++ src/main/java/io/github/rothschil/domain/entity/Intf.java
@@ -1,4 +1,21 @@
 package io.github.rothschil.domain.entity;
 
-public class Intf {
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Builder(toBuilder = true)
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Intf implements Serializable {
+
+    private String callee;
+
+    private String caller;
+
 }
