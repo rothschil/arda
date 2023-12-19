74450de95f0ff3ba7436a334ca05eedda0d266bb
SamAbram <WCNGS@HOTMAIL.COM>
2023-12-19 15:41:32 +0800

    NONE

--- src/main/java/io/github/rothschil/RothschilApplication.java
+++ src/main/java/io/github/rothschil/RothschilApplication.java
@@ -4,7 +4,9 @@ import io.github.rothschil.common.config.Version;
 import io.github.rothschil.common.persistence.jpa.repository.factory.BaseRepositoryFactoryBean;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,7 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Slf4j
 @EnableJpaAuditing
 @EnableJpaRepositories(basePackages = {"io.github.rothschil"},repositoryFactoryBeanClass = BaseRepositoryFactoryBean.class)
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class RothschilApplication {
 
 	public static void main(String[] args) {
